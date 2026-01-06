0fb70394afdd606f2cbee939dd8a01b86243b8d2
pacific-prime777 <borisslava04@gmail.com>
2026-01-06 21:18:48 +0100

    Fix license config for PyPI build

--- setup.py
+++ setup.py
@@ -59,7 +59,6 @@ setup(
         "Framework :: FastAPI",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
